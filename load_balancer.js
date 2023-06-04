a7d82c7d6f5ad46f49e54ba659866ac9bcb010b7
Himanshu Gaur <himanshugaur@Himanshus-MacBook-Air.local>
2023-06-04 09:15:26 +0530

    setting up the default functions

--- load_balancer.js
+++ load_balancer.js
@@ -5,11 +5,17 @@ class LoadBalancer {
         this.nodesCount = nodesCount
         this.nodes = [...Array(nodesCount).keys()].map(v => new ServerNode(v))
         this.bst = createTree()
+        this.partitionToNodeMap = new Map();
     }
     addNode(serverNode) {
         this.nodes.push(serverNode)
     }
     routeRequest(key) {
 
+    }
+    get(key) {
+        var partition = this.bst.lt(key).next();
+        var serverNode = this.partitionToNodeMap.get(partition)
+        console.log(serverNode.get(key))
     }
 }
--- server_node.js
+++ server_node.js
@@ -1,5 +1,11 @@
-class ServerNode{
-    constructor(id){
-        this.id=id
+class ServerNode {
+
+    constructor(id) {
+        this.id = id
+        this.keysMap = new Map()
+        this.partitionToKeys = new Map();
+    }
+    get(key) {
+        return this.keysMap.get(key)
     }
 }
